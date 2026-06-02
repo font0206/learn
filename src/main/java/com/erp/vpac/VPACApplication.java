edf01746cbcfd0b635d962a66f13d746d1ef9030
Phong <phongpt2@imt-soft.com>
2026-06-02 13:27:02 +0700

    create

---
+++ src/main/java/com/erp/vpac/VPACApplication.java
@@ -0,0 +1,13 @@
+package com.erp.vpac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VPACApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VPACApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/erp/vpac/security/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.erp.vpac.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain fillChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/h2-console/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .csrf(csrf -> csrf
+                        .ignoringRequestMatchers("/h2-console/**")
+                )
+                .headers(headers -> headers
+                        .frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin)
+                );
+
+        return http.build();
+    }
+}
